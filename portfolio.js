82de4781487a012bcc1c26a5f7b3f551d044ab0c
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-04 21:48:29 -0800

    consolidated template files

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+var projects = [];
+
+function Project (content) {
+  this.title = content.title;
+  this.url = content.url;
+  this.description = content.description;
+}
+
+$(document).ready(function(){
+
+});
