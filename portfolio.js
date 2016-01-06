5b87d349c11082017cfc92bb313d5aa156cab8c4
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-05 21:03:01 -0800

    added method to constructor prototype, cleaned up html

--- portfolio.js
+++ portfolio.js
@@ -4,7 +4,20 @@ function Project (content) {
   this.title = content.title;
   this.url = content.url;
   this.description = content.description;
+  this.image = content.image;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template').attr('id', this.title);
+  $newProject.find('a').attr('href', this.url);
+  $newProject.find('p').attr(this.description);
+  $newProject.find('img').attr('src', this.image);
+};
+
+data.forEach(function(data) {
+  projects.push(new Project(data));
+});
 
 $(document).ready(function(){
 
