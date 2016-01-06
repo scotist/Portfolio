5e523d385b6f09b5c059604dcdd2bfccc71dbe22
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-05 22:33:56 -0800

    typo fix

--- portfolioview.js
+++ portfolioview.js
@@ -7,7 +7,7 @@ view.handleMainNav = function() {
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
   $('.tab-content').hide();
-  $('#' + $(this).data('tab')).fadeIn();
+  $('#' + $(this).data('content')).fadeIn();
 
   $('.tab').on('click', function(event) {
     event.preventDefault();
