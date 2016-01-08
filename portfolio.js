7abfdd65989c463f5ac4c646de9acc2672de6e28
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-07 22:10:03 -0800

    fiddling with non-working features, only minor changes

--- portfolio.js
+++ portfolio.js
@@ -7,19 +7,6 @@ function Project (content) {
   this.image = content.image;
 }
 
-// Project.prototype.toHtml = function() {
-//   var $newProject = $('article.template').clone();
-//   $newProject.removeClass('template').attr('id', this.title);
-//   $newProject.find('a').attr('href', this.url);
-//   $newProject.find('p').attr(this.description);
-//   $newProject.find('img').attr('src', this.image);
-//   $newProject.find('time[pubdate]').attr('datetime', this.publishedOn);
-//   $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-//   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//   $newProject.append('<hr>');
-//   return $newProject;
-// };
-
 
 Project.prototype.toHtml = function() {
   var templateScript = $('#project-template').html();
