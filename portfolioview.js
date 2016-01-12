bdf15fb7f7529473d4062c38e3abf66dd5cb34d4
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-11 21:38:46 -0800

    refactored js files to upload projects with json, not yet working

--- portfolioview.js
+++ portfolioview.js
@@ -1,47 +1,5 @@
 var projectView = {};
 
-// projectView.populateFilter = function() {
-//   $('project').each(function() {
-//     if(!$(this).hasClass('template')) {
-//       var val = $(this).find('address a').text();
-//       var optionTag = '<option value="' + val + '">' + val + '</option"';
-//       $('#category-filter').append(optionTag);
-//     }
-//   });
-// };
-
-projectView.populateFilter = function() {
-  $('project').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#author-filter').append(optionTag);
-
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
-      }
-    }
-  });
-};
-
-
-projectView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('project').hide();
-      $('project[data-category="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('project').fadeIn();
-      $('project.template').hide();
-    }
-    $('#project-filter').val('');
-  });
-};
-
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function(e) {
     $('.tab-content').hide();
@@ -50,25 +8,37 @@ projectView.handleMainNav = function() {
   $('.main-nav .tab:first').click();
 };
 
-projectView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('#articles').on('click', 'a.read-on', function(e) {
-    e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).hide();
-  });
-};
-
 projectView.menuToggle = function() {
   $('.icon-menu').on('click', function() {
     $('.main-nav ul').slideToggle();
   });
 };
 
+// projectView.initNewProjectPage = function() {
+//   $('tab-content').show();
+//   $('#export-field').hide();
+//   $('#project-json').on('focus', function() {
+//     this.select();
+//   });
+//   $('#new-form').on('change', 'input, textarea', projectView.create);
+// };
+
+// projectView.initIndexPage = function() {
+//   Project.all.forEach(function(a){
+//     $('#projects').append(a.toHtml());
+//   });
+//   projectView.handleMainNav();
+//   projectView.menuToggle();
+// };
+
+$.ajax('projects.json', function() {
+  console.log(xhr);
+});
+
 $(document).ready(function() {
-  projectView.populateFilter();
-  projectView.handleCategoryFilter();
-  // projectView.handleMainNav();
-  projectView.setTeasers();
+  // projectView.populateFilter();
+  // projectView.handleCategoryFilter();
+  projectView.handleMainNav();
+  // projectView.setTeasers();
   projectView.menuToggle();
 });
