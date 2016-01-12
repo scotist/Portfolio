b2baa030536a801aadefbbf85d6dbf71132393f6
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-11 20:11:34 -0800

    took out filter in index, did other minor alterations

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,3 @@
-var projects = [];
-
 function Project (content) {
   this.title = content.title;
   this.url = content.url;
@@ -7,25 +5,48 @@ function Project (content) {
   this.image = content.image;
 }
 
+Project.all = [];
+
 
 Project.prototype.toHtml = function() {
-  var templateScript = $('#project-template').html();
-  var converter = Handlebars.compile(templateScript);
-  var dataSource = this;
-  var theCompiledHtml = converter(dataSource);
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  return theCompiledHtml;
+  var template = Handlebars.compile($('#project-template').text());
+  this.body = marked(this.body);
+  return template(this);
+  // var templateScript = $('#project-template').html();
+  // var converter = Handlebars.compile(templateScript);
+  // var dataSource = this;
+  // var theCompiledHtml = converter(dataSource);
+  // this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  // this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  // return theCompiledHtml;
 };
 
-data.sort(function(a,b){
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+Project.loadAll = function(data) {
+  data.forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
+};
 
-data.forEach(function(ele) {
-  projects.push(new Project(ele));
-});
-
-projects.forEach(function(a){
-  $('#projects').append(a.toHtml());
-});
+Project.fetchAll = function() {
+  if (localStorage.data) {
+    Project.loadAll(JSON.parse(localStorage.data));
+    projectView.initIndexPage();
+  } else {
+    $.getJSON('projects.json', function(data){
+      Project.loadAll(data);
+      localStorage.data = JSON.stringify(data);
+      projectView.initIndexPage();
+    });
+  }
+};
+// data.sort(function(a,b){
+//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
+//
+// data.forEach(function(ele) {
+//   projects.push(new Project(ele));
+// });
+//
+// projects.forEach(function(a){
+//   $('#projects').append(a.toHtml());
+// });
