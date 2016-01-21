8ac605d740b88328ca75f94ed6c3416165fd863a
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-20 20:36:52 -0800

    fixed linting issues

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,21 +1,21 @@
 (function(module) {
 
-function Project(features){
-  Object.keys(features).forEach(function(e, index, keys){
-    this[e] = features[e];
-  }, this);
-};
+  function Project(features){
+    Object.keys(features).forEach(function(e, index, keys){
+      this[e] = features[e];
+    }, this);
+  };
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function(){
-  var template = Handlebars.compile($('#article-template').text());
-  this.body = marked(this.body);
-  return template(this);
-};
+  Project.prototype.toHtml = function(){
+    var template = Handlebars.compile($('#article-template').text());
+    this.body = marked(this.body);
+    return template(this);
+  };
 
-Project.createTable = function(callback) {
-  webDB.execute(
+  Project.createTable = function(callback) {
+    webDB.execute(
     'CREATE TABLE IF NOT EXISTS articles (' +
       'id INTEGER PRIMARY KEY, ' +
       'title VARCHAR(255) NOT NULL, ' +
@@ -27,173 +27,98 @@ Project.createTable = function(callback) {
       if (callback) callback();
     }
   );
-};
+  };
 
-Project.truncateTable = function(callback) {
-  webDB.execute(
+  Project.truncateTable = function(callback) {
+    webDB.execute(
     'DELETE FROM projects;',
     callback
   );
-};
+  };
 
-Project.prototype.insertRecord = function(callback) {
-  webDB.execute(
-  [
-    {
-      'sql': 'INSERT INTO projects (title, category, url, description), VALUES (????);',
-      'data': [this.title, this.category, this.url, this.description],
-    }
-  ],
+  Project.prototype.insertRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO projects (title, category, url, description), VALUES (????);',
+          'data': [this.title, this.category, this.url, this.description],
+        }
+      ],
   callback
   );
-};
+  };
 
-Project.prototype.deleteRecord = function(callback) {
-  webDB.execute(
-    [
-      {
-        'sql': 'DELETE FROM projects WHERE id = ?;',
-        'data': [this.id]
-      }
-    ],
-    callback
-  );
-};
+  Project.prototype.deleteRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'DELETE FROM projects WHERE id = ?;',
+          'data': [this.id]
+        }
+      ],
+      callback
+    );
+  };
 
-Project.prototype.updateRecord = function(callback) {
-  webDB.execute(
-    [
-      {
-        'sql': 'UPDATE projects SET title = ?, category = ?, url = ?, description = ? WHERE id = ?;',
-        'data': [this.title, this.category, this.url, this.description, this.id]
-      }
-    ],
-    callback
-  );
-};
+  Project.prototype.updateRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'UPDATE projects SET title = ?, category = ?, url = ?, description = ? WHERE id = ?;',
+          'data': [this.title, this.category, this.url, this.description, this.id]
+        }
+      ],
+      callback
+    );
+  };
 
-////////////////////////
-////////////////////////
-Project.loadAll = function(rows){
-  Project.all = rows.map(function(ele) {
-    return new Project(ele);
-  });
-};
+  ////////////////////////
+  ////////////////////////
+  Project.loadAll = function(rows){
+    Project.all = rows.map(function(ele) {
+      return new Project(ele);
+    });
+  };
 
-// Project.checkUpdates = function(){
-//   $.getJSON('projects.json', function(rawData){
-//     Project.loadAll(rawData);
-//     localStorage.rawData = JSON.stringify(rawData);
-//     projectView.initProjects();
-//   });
-// };
-//
-// Project.checkEtags = function(data, message, xhr){
-//   var eTag = xhr.getResponseHeader('ETag');
-//   if(typeof localStorage.projEtag == 'undefined' || localStorage.projEtag != eTag){
-//     localStorage.projEtag = eTag;
-//     Project.checkUpdates();
-//   }else {
-//     Project.loadAll(JSON.parse(localStorage.rawData));
-//     projectView.initProjects();
-//   }
-// };
+  // Project.checkUpdates = function(){
+  //   $.getJSON('projects.json', function(rawData){
+  //     Project.loadAll(rawData);
+  //     localStorage.rawData = JSON.stringify(rawData);
+  //     projectView.initProjects();
+  //   });
+  // };
+  //
+  // Project.checkEtags = function(data, message, xhr){
+  //   var eTag = xhr.getResponseHeader('ETag');
+  //   if(typeof localStorage.projEtag == 'undefined' || localStorage.projEtag != eTag){
+  //     localStorage.projEtag = eTag;
+  //     Project.checkUpdates();
+  //   }else {
+  //     Project.loadAll(JSON.parse(localStorage.rawData));
+  //     projectView.initProjects();
+  //   }
+  // };
 
-Project.fetchAll = function(next){
-  webDB.execute('SELECT * FROM projects', function(rows_){
-    if (rows.length) {
-      Project.loadAll(rows);
-      next();
-    } else {
-      $.getJSON('projects.json', function(rawData){
-        rawData.forEach(function(item){
-          var project = new Project(item);
-          project.insertRecord();
+  Project.fetchAll = function(next){
+    webDB.execute('SELECT * FROM projects', function(rows_){
+      if (rows.length) {
+        Project.loadAll(rows);
+        next();
+      } else {
+        $.getJSON('projects.json', function(rawData){
+          rawData.forEach(function(item){
+            var project = new Project(item);
+            project.insertRecord();
+          });
+          webDB.execute('SELECT * FROM projects', function(rows){
+            Project.loadAll(rows);
+            next();
+          });
         });
-        webDB.execute('SELECT * FROM projects', function(rows){
-          Project.loadAll(rows);
-          next();
-        });
-      });
-    }
-  });
-};
+      }
+    });
+  };
 
 
-module.Project = Project;
+  module.Project = Project;
 })(window);
-
-// function Project (content) {
-//   this.title = content.title;
-//   this.url = content.url;
-//   this.description = content.description;
-//   this.image = content.image;
-// }
-//
-// Project.all = [];
-//
-// Project.prototype.toHtml = function(a) {
-//   var source = $('#project-template').html();
-//   var template = Handlebars.compile(source);
-//   return html;
-// };
-//
-// Project.loadAll = function(data) {
-//   data.forEach(function(ele){
-//     Project.all.push(new Project(ele));
-//   });
-// };
-//
-// Project.checkUpdates = function(){
-//   $.getJSON('projects.json', function(data){
-//     Project.loadAll(data);
-//     localStorage.data = JSON.stringify(data);
-//     projectView.initProjects();
-//   }).error(function(jqXHR, textStatus, errorThrown) {
-//         console.log("error " + textStatus);
-//         console.log("incoming Text " + jqXHR.responseText);
-//     })
-//
-// };
-//
-// Project.checkEtags = function(data, message, xhr){
-//   var eTag = xhr.getResponseHeader('ETag');
-//   if(typeof localStorage.projEtag == 'undefined' || localStorage.projEtag != eTag){
-//     localStorage.projEtag = eTag;
-//     Project.checkUpdates();
-//   }else {
-//     Project.loadAll(JSON.parse(localStorage.data));
-//     projectView.initIndexPage();
-//   }
-// };
-//
-// Project.fetchAll = function(){
-//   if(localStorage.data){
-//     $.getJSON('projects.json', function(data){
-//       Project.loadAll(data);
-//       localStorage.data = JSON.stringify(data);
-//     });
-//     // var head = $.ajax({
-//     //   type: 'HEAD',
-//     //   url: 'projects.json',
-//     //   success: Project.checkEtags
-//     // });
-//   } else {
-//     Project.checkUpdates();
-//   }
-// };
-//
-//
-// // Project.fetchAll = function() {
-// //   if (localStorage.data) {
-// //   Project.loadAll(JSON.parse(localStorage.data));
-// //   projectView.initIndexPage();
-// // } else {
-// //   console.log('something happening');
-// //   $.getJSON('projects.json').done(function(data){
-// //     Project.loadAll(data);
-// //     localStorage.data = JSON.stringify(data);
-// //     projectView.initIndexPage();
-// //   });
-// //   }
-// // };
--- scripts/portfolioview.js
+++ scripts/portfolioview.js
@@ -1,94 +1,41 @@
 (function(module) {
 
-var projectView = {};
+  var projectView = {};
 
-projectView.initNewProjectPage = function () {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#project-json').on('focus', function(){
-    this.select();
-  });
-  $('#new-form').on('change', 'input, textarea', projectView.create);
-};
+  projectView.initNewProjectPage = function () {
+    $('.tab-content').show();
+    $('#export-field').hide();
+    $('#project-json').on('focus', function(){
+      this.select();
+    });
+    $('#new-form').on('change', 'input, textarea', projectView.create);
+  };
 
-projectView.create = function() {
-  var project;
-  $('#projects').empty();
+  projectView.create = function() {
+    var project;
+    $('#projects').empty();
 
-  project = new Project({
-    title: $('#project-title').val(),
-    category: $('#project-category').val(),
-    url: $('#project-url').val(),
-    description: $('#project-description').val()
-  });
+    project = new Project({
+      title: $('#project-title').val(),
+      category: $('#project-category').val(),
+      url: $('#project-url').val(),
+      description: $('#project-description').val()
+    });
 
-  $('#projects').append(project.toHtml());
-  $('pre code').each(function(i, block) {
-    hljs.highlight(block);
-  });
+    $('#projects').append(project.toHtml());
+    $('pre code').each(function(i, block) {
+      hljs.highlight(block);
+    });
 
-  $('#export-field').show();
-  $('#project-json').val(JSON.stringify(project) + ',');
-};
+    $('#export-field').show();
+    $('#project-json').val(JSON.stringify(project) + ',');
+  };
 
-projectView.initIndexPage = function() {
-  Project.all.forEach(function(a){
-    $('#projects').append(a.toHtml());
-  });
-};
+  projectView.initIndexPage = function() {
+    Project.all.forEach(function(a){
+      $('#projects').append(a.toHtml());
+    });
+  };
 
-module.projectView = projectView;
+  module.projectView = projectView;
 })(window);
-
-// projectView.handleMainNav = function(){
-//   $('#main-nav').on('click', '.tab', function(e) {
-//     $('section').hide();
-//     $('#' + $(this).data('content')).fadeIn();
-//   });
-//
-//   $('#main-nav .tab:first').click();
-// };
-//
-// projectView.menuToggle = function() {
-//   $('.icon-menu').on('click', function() {
-//     $('.main-nav ul').slideToggle();
-//   });
-// };
-
-// projectView.initProjects = function(){
-//   Project.all.forEach(function(a){
-//     $('#projects').append(a.toHtml(a));
-//   });
-//   projectView.handleMainNav();
-//   projectView.menuToggle();
-// };
-//
-// $(document).ready(function() {
-//   projectView.handleMainNav();
-//   projectView.menuToggle();
-// });
-
-
-// projectView.handleMainNav = function() {
-//   $('.main-nav').on('click', '.tab', function(e) {
-//     $('.tab-content').hide();
-//     $('#' + $(this).data('content')).fadeIn();
-//   });
-//   $('.main-nav .tab:first').click();
-// };
-//
-
-//
-//
-// projectView.initIndexPage = function() {
-//   Project.all.forEach(function(a){
-//     $('#projects').append(a.toHtml(a));
-//   });
-//   projectView.handleMainNav();
-//   projectView.menuToggle();
-// };
-//
-// $(document).ready(function() {
-//   projectView.handleMainNav();
-//   projectView.menuToggle();
-// });
